b7f4dc9e2081cd498043815bff8109cb5fe636c8
CarlosMeyer2021 <83259498+CarlosMeyer2021@users.noreply.github.com>
2022-05-05 19:25:16 -0300

    portifolio v1.0

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Oi meu nome é</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Carlos Meyer</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Sou Desenvolvedor Full Stack.</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>Sou um Desenvolvedor Full Stack especializado em construir (e ocasionalmente projetar) experiências digitais excepcionais. Atualmente, estou focado na construção de aplicativos web full-stack responsivos.</p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>Ver trabalho
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3' />
+            </span>
+          </button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from 'react'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import Logo from '../assets/logo.png'
+import Home from '../assets/home.png'
+import About from '../assets/about.png'
+import Skills from '../assets/skills.png'
+import Work from '../assets/work.png'
+import Contact from '../assets/contact.png'
+import { Link } from 'react-scroll'
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => {
+    setNav(!nav)
+  }
+
+
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
+      </div>
+
+      {/* menu */}
+      <ul className='hidden md:flex'>
+        <li>
+          <Link to="home" spy={true} smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" spy={true} smooth={true} duration={500}>
+            Sobre
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" spy={true} smooth={true} duration={500}>
+            Habilidades
+          </Link>
+        </li>
+        <li>
+          <Link to="work" spy={true} smooth={true} duration={500}>
+            Trabalho
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" spy={true} smooth={true} duration={500}>
+            Contato
+          </Link>
+        </li>
+      </ul>
+
+      {/* Hamburgues */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <div className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <ul >
+          <li className='py-6 text-4xl'>
+            <Link className='flex justify-center items-cente' onClick={handleClick} to="home" spy={true} smooth={true} duration={500}>
+              <img className='mb-3 mr-2' src={Home} alt='Home icon' style={{ width: '35px' }} />
+              Home
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'>
+            <Link className='flex justify-center items-center' onClick={handleClick} to="about" spy={true} smooth={true} duration={500}>
+              <img className='mb-3 mr-2' src={About} alt='About icon' style={{ width: '35px' }} />
+              Sobre
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'>
+            <Link className='flex justify-center items-center' onClick={handleClick} to="skills" spy={true} smooth={true} duration={500}>
+              <img className='mb-3 mr-2' src={Skills} alt='Skills icon' style={{ width: '35px' }} />
+              Habilidades
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'>
+            <Link className='flex justify-center items-center' onClick={handleClick} to="work" spy={true} smooth={true} duration={500}>
+              <img className='mb-3 mr-2' src={Work} alt='work icon' style={{ width: '35px' }} />
+              Trabalho
+            </Link>
+          </li>
+          <li className='py-6 text-4xl'>
+            <Link className='flex justify-center items-center' onClick={handleClick} to="contact" spy={true} smooth={true} duration={500}>
+              <img className='mb-3 mr-2' src={Contact} alt='contact icon' style={{ width: '35px' }} />
+              Contato
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Social icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a className='flex justify-between items-end w-full text-gray-300' href='https://www.linkedin.com/in/carlosaugustomeyer/' target="_blank">
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a className='flex justify-between items-end w-full text-gray-300' href='https://github.com/CarlosMeyer2021' target="_blank">
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a className='flex justify-between items-end w-full text-gray-300' href='/'>
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a className='flex justify-between items-end w-full text-gray-300' href='/'>
+              Rusume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
