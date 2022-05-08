48a792643855bc7e92597e31144eab230b8d0898
CarlosMeyer2021 <83259498+CarlosMeyer2021@users.noreply.github.com>
2022-05-08 14:43:13 -0300

    portifolio v1.1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
-import Logo from '../assets/logo.png'
+import Logo from '../assets/logocm.png'
 import Home from '../assets/home.png'
 import About from '../assets/about.png'
 import Skills from '../assets/skills.png'
@@ -19,8 +19,8 @@ const Navbar = () => {
 
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-      <div>
-        <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
+      <div className='justify-start items-center'>
+        <img src={Logo} alt='Logo Image' style={{ width: '180px' }} />
       </div>
 
       {/* menu */}
@@ -61,31 +61,31 @@ const Navbar = () => {
       <div className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
         <ul >
           <li className='py-6 text-4xl'>
-            <Link className='flex justify-center items-cente' onClick={handleClick} to="home" spy={true} smooth={true} duration={500}>
+            <Link className='flex justify-start items-center' onClick={handleClick} to="home" spy={true} smooth={true} duration={500}>
               <img className='mb-3 mr-2' src={Home} alt='Home icon' style={{ width: '35px' }} />
               Home
             </Link>
           </li>
           <li className='py-6 text-4xl'>
-            <Link className='flex justify-center items-center' onClick={handleClick} to="about" spy={true} smooth={true} duration={500}>
+            <Link className='flex justify-start items-center' onClick={handleClick} to="about" spy={true} smooth={true} duration={500}>
               <img className='mb-3 mr-2' src={About} alt='About icon' style={{ width: '35px' }} />
               Sobre
             </Link>
           </li>
           <li className='py-6 text-4xl'>
-            <Link className='flex justify-center items-center' onClick={handleClick} to="skills" spy={true} smooth={true} duration={500}>
+            <Link className='flex justify-start items-center' onClick={handleClick} to="skills" spy={true} smooth={true} duration={500}>
               <img className='mb-3 mr-2' src={Skills} alt='Skills icon' style={{ width: '35px' }} />
               Habilidades
             </Link>
           </li>
           <li className='py-6 text-4xl'>
-            <Link className='flex justify-center items-center' onClick={handleClick} to="work" spy={true} smooth={true} duration={500}>
+            <Link className='flex justify-start items-center' onClick={handleClick} to="work" spy={true} smooth={true} duration={500}>
               <img className='mb-3 mr-2' src={Work} alt='work icon' style={{ width: '35px' }} />
               Trabalho
             </Link>
           </li>
           <li className='py-6 text-4xl'>
-            <Link className='flex justify-center items-center' onClick={handleClick} to="contact" spy={true} smooth={true} duration={500}>
+            <Link className='flex justify-start items-center' onClick={handleClick} to="contact" spy={true} smooth={true} duration={500}>
               <img className='mb-3 mr-2' src={Contact} alt='contact icon' style={{ width: '35px' }} />
               Contato
             </Link>
