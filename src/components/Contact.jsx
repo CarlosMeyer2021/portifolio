0cdbcb8fe664b1341b50c43546a67526b025ce38
CarlosMeyer2021 <83259498+CarlosMeyer2021@users.noreply.github.com>
2022-05-30 01:33:39 -0300

    portfolio v1.07

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -66,14 +66,14 @@ const Contact = () => {
 
 
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 mb-4'>
+    <div name='contact' className='w-full h-full bg-[#0a192f] flex justify-center items-center p-4 mb-4'>
 
       {Object.keys(formErros).length === 0 && isSubmit ? (<div className='ui message success'>{validaCampos}</div>) : (
         <pre className='hidden'>{JSON.stringify(formValues, undefined, 2)}</pre>
       )}
 
 
-      <form onSubmit={handleSubmit} method='POST' className='flex flex-col max-w-[600px] w-full pt-20 pb-20'>
+      <form onSubmit={handleSubmit} method='POST' className='flex flex-col max-w-[600px] w-full pt-24 pb-20'>
         <ToastContainer />
         <div className='p-8'>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contato</p>
