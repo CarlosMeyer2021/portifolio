cff5d269daf67e674d4fe900c5246475109276f8
CarlosMeyer2021 <83259498+CarlosMeyer2021@users.noreply.github.com>
2022-06-13 21:22:20 -0300

    portfolio v2.05

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { HiArrowNarrowRight } from 'react-icons/hi'
 const Home = () => {
 
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f] sm:mt-5'>
+    <div name='home' className='w-full h-screen bg-[#0a192f] mt-10'>
 
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-[600px] py-5'>
